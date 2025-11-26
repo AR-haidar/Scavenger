a0bd6c30233391777e2d3e048304857c1b20f0b8
AR-haidar <haidaratqiyarahman2@gmail.com>
2025-11-26 10:30:37 +0700

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Scavenger</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid d-flex justify-content-center align-items-center"
+        style="background: #44BBA4; min-height: 100vh">
+        <div class="content">
+            <img src="{{ asset('assets/scavengerLogo2.png') }}" alt="" style="width:442px">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body ">
+                    <h1 class="h1">Welcome</h1>
+                    <div class="">
+                        <input type="text" name="" id="" class="form-control" placeholder="Email">
+                        <input type="text" name="" id="" class="form-control" placeholder="Password">
+                        <button class="btn btn-primary w-100">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
