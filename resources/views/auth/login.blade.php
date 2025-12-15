e195f6a3f80f1bb6f97b59f62b39e6293c1724f8
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-12-15 14:19:59 +0700

    Modifikasi Login Page, Menambahkan Login with Google, Routing sesuai halaman

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,23 +25,48 @@
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <div class="my-4 flex justify-between">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
-        </div>
 
-        <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+            
+        </div>
+        
 
-            <x-primary-button class="ms-3">
+       
+        <x-primary-button class="w-full flex items-center justify-center px-4 py-3 my-2 bg-black text-white rounded-lg">
                 {{ __('Log in') }}
             </x-primary-button>
-        </div>
     </form>
+
+
+    <form method="GET" action="#">
+    <x-secondary-button
+    class="w-full flex items-center justify-center my-2 px-4 py-3
+           bg-white text-black border border-black rounded-lg
+           transition
+           hover:bg-grey-400 hover:text-black
+           ">
+     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="17" height="17" viewBox="0 0 48 48">
+<path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+</svg>
+    <span class="ms-1">LOGIN WITH GOOGLE</span>
+</x-secondary-button>
+
+
+</form>
+
+
+        
+    <div class="text-sm flex items-center justify-center my-4">
+        Doesn't have an account yet? 
+        <a href="{{ route('register') }}" class="ms-1 text-sm text-blue-500 dark:text-blue-500">{{ __('Create Account') }}</a>
+    </div>
 </x-guest-layout>
