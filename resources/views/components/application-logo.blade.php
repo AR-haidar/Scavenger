aae147ab9b929db47a6f9a0299888e44aa460333
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-12-15 16:26:58 +0700

    Minor update

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,7 @@
 @if (request()->routeIs('login'))
     <p class="text-lg text-center text-gray-500">Login to</p>
 @endif
+@if (request()->routeIs('register'))
+    <p class="text-lg text-center text-gray-500">Register to</p>
+@endif
 <img src="/assets/scavengerLogo.png" alt="" {{ $attributes->merge(['class' => 'h-12 w-auto mx-auto']) }}>
