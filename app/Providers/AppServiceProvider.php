a7837bb8a729d58fa9fc645baa36f902f91239d1
Fadhil Anwar <fadhilanwar42@gmail.com>
2025-12-16 13:40:56 +0700

    Menambahkan rule point Service Provider agar mengakses HTTP(S) saat running di Ngrok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale('id');
+
+        // Jangan dihapus ya, ini buat ngrok biar jalan di local
+        if($this->app->environment('local') && !str_contains(config('app.url'), '127.0.0.1') && !str_contains(config('app.url'), 'localhost')) {
+            URL::forceScheme('https');
+        }
     }
 }
